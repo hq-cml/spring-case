3418279b43ffe11f93c68045c5774534bad1dddd
hq <hq_cml@163.com>
2023-07-13 11:47:15 +0800

    SpringBoot thymeleaf常用标签

---
+++ springboot-thymeleaf/src/main/java/com/hq/entity/User.java
@@ -0,0 +1,10 @@
+package com.hq.entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private Integer id;
+    private String name;
+}
